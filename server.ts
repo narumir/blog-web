39177d832c65253bfa6d4e2b1110cd51d3d1283e
narumir <narumir@narumir.io>
2023-03-02 07:58:49 +0000

    deploy aws serverless lambda

---
+++ server.ts
@@ -0,0 +1,25 @@
+import serverless from "serverless-http";
+import NextServer from "next/dist/server/next-server";
+// @ts-ignore
+import { config } from "./.next/required-server-files.json";
+import type {
+    NextConfig,
+} from "next";
+import type {
+    Options,
+} from "serverless-http";
+
+const server = new NextServer({
+    hostname: "localhost",
+    port: 3000,
+    dir: __dirname,
+    dev: false,
+    conf: {
+        ...(config as NextConfig),
+    },
+});
+const serverlessOption: Options = {
+    binary: ["*/*"]
+};
+
+export const handler = serverless(server.getRequestHandler(), serverlessOption);
