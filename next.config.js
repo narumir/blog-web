6e21fa862f13e6bd0cfe2c0658bb4335bc6a001b
narumir <narumir@narumir.io>
2023-03-02 06:47:02 +0000

    update nextjs out standalone

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ *  @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    unoptimized: true,
+  },
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
