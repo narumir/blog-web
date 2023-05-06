99a4d06572963f7da0439e3d3dd2b74e4031eebf
narumir <narumir@narumir.io>
2023-05-06 13:21:01 +0000

    add lambda function build

--- build.sh
+++ build.sh
@@ -2,14 +2,19 @@
 
 BUILD_FOLDER=.dist
 
-next build
+# Clean
 rm -rf $BUILD_FOLDER
+
+# Next.js build and copy
+next build
 mv .next/standalone/ $BUILD_FOLDER/
 cp -r .next/static $BUILD_FOLDER/.next
-rm $BUILD_FOLDER/server.js
 cp -r next.config.js $BUILD_FOLDER/
-cp serverless.yml $BUILD_FOLDER/
-cp server.ts $BUILD_FOLDER/
 cp -r public $BUILD_FOLDER/
+
+# Build lambda function
+cp server.ts $BUILD_FOLDER/
 cd $BUILD_FOLDER
-sls deploy
+npm run build:lambda
+rm server.ts
+
