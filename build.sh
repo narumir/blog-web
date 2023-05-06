744359d79dcbf5b976118f2a59886765639163a8
narumir <narumir@narumir.io>
2023-05-06 13:30:36 +0000

    update build script

--- build.sh
+++ build.sh
@@ -4,17 +4,14 @@ BUILD_FOLDER=.dist
 
 # Clean
 rm -rf $BUILD_FOLDER
+rm -rf .next
 
 # Next.js build and copy
-next build
+npm run build:next
 mv .next/standalone/ $BUILD_FOLDER/
 cp -r .next/static $BUILD_FOLDER/.next
 cp -r next.config.js $BUILD_FOLDER/
 cp -r public $BUILD_FOLDER/
 
 # Build lambda function
-cp server.ts $BUILD_FOLDER/
-cd $BUILD_FOLDER
 npm run build:lambda
-rm server.ts
-
