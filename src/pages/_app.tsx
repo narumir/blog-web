290fd3c3f397d9c4e9187bbe197de3d6a1f3271d
narumir <narumir@narumir.io>
2023-07-08 06:49:13 +0000

    remove emotion package and replace heaer with tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,20 @@
-import createCache from '@emotion/cache';
-import globalStyle from "src/global-style";
 import {
-  CacheProvider,
-  Global,
-} from '@emotion/react';
+  Fragment,
+} from 'react';
 import {
-  NavBar,
-} from 'src/views/components';
+  Header,
+} from 'src/components';
 import type {
   AppProps,
 } from 'next/app'
-
-const cache = createCache({ key: "next" });
+import "src/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <CacheProvider value={cache}>
-      <Global styles={globalStyle} />
-      <NavBar />
+    <Fragment>
+      <Header />
       <Component {...pageProps} />
-    </CacheProvider>
+    </Fragment>
   );
 };
 
