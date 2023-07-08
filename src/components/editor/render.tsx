1646f578172050f6d13fafe78fd97d29da22f3bb
narumir <narumir@narumir.io>
2023-07-08 13:09:32 +0000

    apply sanitize html

--- src/components/editor/render.tsx
+++ src/components/editor/render.tsx
@@ -1,3 +1,4 @@
+import sanitize from "sanitize-html";
 import {
   OutputData,
 } from "@editorjs/editorjs";
@@ -6,6 +7,12 @@ export const renderEditorJS = (data: OutputData) =>
   data.blocks.map((block) => {
     switch (block.type) {
       case "paragraph":
-        return (<p key={block.id} dangerouslySetInnerHTML={{ __html: block.data.text }} />);
+        const clean = sanitize(block.data.text);
+        return (<p key={block.id} dangerouslySetInnerHTML={{ __html: clean }} />);
     }
   });
+
+/**
+ * 나중에 적용하면 좋은 코드 브라우저단에서 지원하는 sanitizer
+ * https://developer.mozilla.org/en-US/docs/Web/API/HTML_Sanitizer_API
+ */
