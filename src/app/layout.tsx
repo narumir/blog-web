6c55a8c6b70b0d7483112cf731f95e091632ff94
narumir <narumir@narumir.io>
2023-07-25 05:47:41 +0000

    add meta code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="ko">
       <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
         {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" /> */}
         {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" /> */}
