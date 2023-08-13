9d2d79908791757ab636ecacf70ca830f16b8549
narumir <narumir@narumir.io>
2023-08-13 08:39:12 +0000

    include image_loader.js in build script

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ cp -r .next/static $STATIC_FOLDER/_next/static
 # Copy serverless
 cp -r .next/standalone/. $BUILD_FOLDER/
 cp -r next.config.js $BUILD_FOLDER/
+cp -r image_loader.js $BUILD_FOLDER/
 
 # Build serverless hanlder
 npm run build:lambda
