30bbc1434ff373aefa98cc7c4396cc40730d5d82
narumir <narumir@narumir.io>
2023-08-15 01:07:32 +0000

    merge footer back to top

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import {
-  FooterBackToTop,
-} from "./back-to-top";
+  useCallback,
+} from "react";
+import {
+  ChevronUp,
+} from "src/icon";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const onClick = useCallback(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
   return (
     <footer className="border-t-[1px] w-full">
       <div className="container m-auto p-5">
-        <FooterBackToTop />
+        <div className="cursor-pointer flex items-center text-lg text-[#808080] mb-5" onClick={onClick}>
+          <ChevronUp className="mr-2" /> back to top
+        </div>
         <hr className="mb-16" />
         <div>&copy; {currentYear} &middot; <Link className="text-blue-500" href={"https://blog.narumir.io"}>narumir.io</Link> &middot; All rights reserved.</div>
       </div>
