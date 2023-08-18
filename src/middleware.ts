1275187e53846443d2703329e41edc40b2a6ef33
narumir <narumir@narumir.io>
2023-08-18 18:09:38 +0000

    refacotoring renew token

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,47 +11,53 @@ import {
   refreshTokenCookieName,
 } from "./utils";
 
+const renewToken = async (token: string, url: string, res: NextResponse) => {
+  const fetchOption: RequestInit = {
+    method: "POST",
+    body: JSON.stringify({ token }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch(url, fetchOption);
+  if (response.status !== 200 && response.status !== 201) {
+    throw new Error("fail to renew token.");
+  }
+  const data = await response.json();
+  if (data.refreshToken != null) {
+    res.cookies.set(refreshTokenCookieName, data.refreshToken, { ...defaultCookieOptions, expires: dayjs(data.refreshTokenExpiredAt).toDate() });
+  }
+  if (data.accessToken != null) {
+    res.cookies.set(accessTokenCookieName, data.accessToken as string, { ...defaultCookieOptions, expires: dayjs(data.accessTokenExpiredAt).toDate() });
+  }
+  return data;
+};
+
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next();
   const cookiesStore = req.cookies;
   let refreshToken = cookiesStore.get(refreshTokenCookieName)?.value;
   if (!cookiesStore.has(refreshTokenCookieName) || refreshToken == null) {
-    return NextResponse.next();
+    return res;
   }
   const current = dayjs();
   const decodeRefreshToken = decodeToken(refreshToken);
   const refreshTokenExpires = dayjs(decodeRefreshToken["exp"] * 1000);
   if (current.isAfter(refreshTokenExpires.add(-1, "hour"))) {
-    const data = await fetch(`${baseURL}/auth/refresh-token`, {
-      method: "POST",
-      body: JSON.stringify({ token: refreshToken }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json());
+    const data = await renewToken(refreshToken, `${baseURL}/auth/refresh-token`, res);
     refreshToken = data.refreshToken as string;
-    res.cookies.set(refreshTokenCookieName, refreshToken, { ...defaultCookieOptions, expires: dayjs(data.refreshTokenExpiredAt).toDate() });
   }
-  const renewAccessToken = async () => {
-    const data = await fetch(`${baseURL}/auth/access-token`, {
-      method: "POST",
-      body: JSON.stringify({ token: refreshToken }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json());
-    res.cookies.set(accessTokenCookieName, data.accessToken as string, { ...defaultCookieOptions, expires: dayjs(data.accessTokenExpiredAt).toDate() });
-    res.headers.set("authorization", data.accessToken);
-    return res;
-  };
   let accessToken = cookiesStore.get(accessTokenCookieName)?.value;
   if (!cookiesStore.has(accessTokenCookieName) || accessToken == null) {
-    return await renewAccessToken();
+    const data = await renewToken(refreshToken, `${baseURL}/auth/access-token`, res);
+    res.headers.set("authorization", data.accessToken);
+    return res;
   }
   const decodeAccessToken = decodeToken(accessToken);
   const accessTokenExpiredAt = dayjs(decodeAccessToken["exp"] * 1000);
   if (current.isAfter(accessTokenExpiredAt.add(-1, "minute"))) {
-    return await renewAccessToken();
+    const data = await renewToken(refreshToken, `${baseURL}/auth/access-token`, res);
+    accessToken = data.accessToken as string;
   }
   res.headers.set("authorization", accessToken);
   return res;
