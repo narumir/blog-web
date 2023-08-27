a7f7c84c920717e9e0632515a81b11c84e3eeae7
narumir <narumir@narumir.io>
2023-08-27 05:13:03 +0000

    remove transition and apply http response format

--- src/app/signin/signin.tsx
+++ src/app/signin/signin.tsx
@@ -2,7 +2,6 @@
 
 import {
   FC,
-  useTransition,
 } from "react";
 import {
   SubmitHandler,
@@ -18,19 +17,17 @@ type Props = {
   publicKey: string;
 }
 const Signin: FC<Props> = ({ publicKey }) => {
-  const [isPending, startTransition] = useTransition();
   const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
   const onSubmit: SubmitHandler<LoginForm> = (body) => {
-    startTransition(() => {
-      body.password = encryptPassword(publicKey, body.password);
-      signin(body).then((result) => {
-        if (result.success === true) {
+    body.password = encryptPassword(publicKey, body.password);
+    signin(body)
+      .then(({ data, error }) => {
+        if (data.success === true) {
           window.location.href = "/";
           return;
         }
         alert("로그인에 실패하였습니다.");
       });
-    });
   };
   return (
     <form className="my-32 w-72 sm:w-1/3 m-auto" onSubmit={handleSubmit(onSubmit)}>
@@ -38,7 +35,7 @@ const Signin: FC<Props> = ({ publicKey }) => {
       {errors.username?.type === "required" && <p className="text-red-500">유저명을 입력해주세요.</p>}
       <input className="block w-full h-12 border-b-[1px] mt-8" placeholder="비밀번호" type="password" {...register("password", { required: true })} />
       {errors.password?.type === "required" && <p className="text-red-500">비밀번호를 입력해주세요.</p>}
-      <input className="block w-full h-12 my-2 bg-blue-500 text-white" type="submit" disabled={isPending} value="로그인" />
+      <input className="block w-full h-12 my-2 bg-blue-500 text-white" type="submit" value="로그인" />
     </form>
   );
 };
