c36ee7b02c5409849c370dce2f2fe9abb24c58be
narumir <narumir@narumir.io>
2023-09-15 14:09:03 +0000

    change buger margin

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -23,11 +23,11 @@ export function Sidebar() {
   const style = isSideExpend ? "w-64" : "w-24";
   return (
     <aside className={cn("hidden md:block h-full border-r-2", style)}>
-      <div className="m-10 flex justify-start items-center">
-        <button className="w-8 h-5 inline-block" aria-label="menu expend" onClick={onBuggerClick}>
+      <div className="my-10 mx-8 flex justify-start items-center">
+        <button className="w-8 h-8 inline-block" aria-label="menu expend" onClick={onBuggerClick}>
           <BuggerIcon />
         </button>
-        <Link href={"/"} className="inline-block ml-6">
+        <Link href={"/"} className="inline-block ml-6 h-8">
           <Image width={100} height={32} src={"/logo.webp"} alt="logo" />
         </Link>
       </div>
