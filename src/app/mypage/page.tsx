69e46bcfc62ce550b1999988b2fa617155cd6c10
narumir <narumir@narumir.io>
2023-09-22 11:40:41 +0000

    add mfa layout

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import {
+  ArrowRightIcon,
+} from "src/icons";
+
+const manageContents = [
+  {
+    name: "My posts",
+    href: "/mypage/posts",
+  },
+  {
+    name: "My series",
+    href: "/mypage/series",
+  },
+  {
+    name: "My assets",
+    href: "/mypage/assets",
+  },
+]
+
+const securitySettings = [
+  {
+    name: "Signin history",
+    href: "/mypage/signin-history",
+  },
+  {
+    name: "Manage MFA",
+    href: "/mypage/mfa",
+  },
+  {
+    name: "Register Keypass",
+    href: "/mypage/keypass",
+  },
+  {
+    name: "Deactive account",
+    href: "/mypage/deactive",
+  },
+];
+
+export default function MyPage() {
+  return (
+    <div className="p-4">
+      <div className="w-full md:max-w-[760px] m-auto rounded-2xl bg-white mb-4">
+        <h2 className="text-xl p-4 font-bold">Manage contents</h2>
+        <ul>
+          {manageContents.map((setting, i) => (
+            <li className="px-4 py-3 border-b-[1px]" key={i}>
+              <Link className="flex justify-between items-center" href={setting.href}>
+                {setting.name}
+                <ArrowRightIcon className="text-gray-500" />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-full md:max-w-[760px] m-auto rounded-2xl bg-white mb-4">
+        <h2 className="text-xl p-4 font-bold">Securty settings</h2>
+        <ul>
+          {securitySettings.map((setting, i) => (
+            <li className="px-4 py-3 border-b-[1px]" key={i}>
+              <Link className="flex justify-between items-center" href={setting.href}>
+                {setting.name}
+                <ArrowRightIcon className="text-gray-500" />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
