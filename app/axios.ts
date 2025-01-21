760694decc11e8212f0427ecef48ccb32f9b26ac
narumir <narumir@narumir.io>
2025-01-21 08:13:25 +0000

    remove unused files

--- app/axios.ts
+++ app/axios.ts
@@ -4,4 +4,5 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_API_SERVER,
   withCredentials: true,
 });
+
 export default instance;
--- app/components/article-list/index.ts
+++ app/components/article-list/index.ts
@@ -1,3 +1,2 @@
 export * from "./article-list";
 export * from "./article-list-item";
-export * from "./article-list-more";
